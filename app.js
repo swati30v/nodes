7054fc2a3a9e45aa6d0c37c383478a9dcb4d4d1e
swati30v <swati30v@gmail.com>
2020-01-20 12:13:14 +0530

    fsf

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 
 app.use(require("./controller/default"))
 
-app.listen(process.env.PORT|| 2500,function(){
+app.listen(process.env.PORT || 2500,function(){
     var obj={};
     obj.name="swati"
     console.log(obj)
